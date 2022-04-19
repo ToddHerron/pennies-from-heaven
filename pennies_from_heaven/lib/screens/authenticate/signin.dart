0bb95f270d628dfd7c19e9101f07b27b10601a28
Todd Herron <todd.c.herron@gmail.com>
2022-04-18 21:03:14 -0600

    Fix Scroll overflow when web window is shrunk

--- pennies_from_heaven/lib/screens/authenticate/signin.dart
+++ pennies_from_heaven/lib/screens/authenticate/signin.dart
@@ -39,45 +39,74 @@ class _SignInState extends State<SignIn> {
         padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              const SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(
-                  prefixIcon: const Icon(Icons.email),
-                  labelText: 'Email',
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                    prefixIcon: const Icon(Icons.email),
+                    labelText: 'Email',
+                  ),
+                  onChanged: (value) {
+                    setState(() => email = value);
+                  },
+                  validator: (value) =>
+                      value!.isEmpty ? 'Enter an email' : null,
                 ),
-                onChanged: (value) {
-                  setState(() => email = value);
-                },
-                validator: (value) => value!.isEmpty ? 'Enter an email' : null,
-              ),
-              const SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(
-                  prefixIcon: const Icon(Icons.lock),
-                  labelText: 'Password',
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                    prefixIcon: const Icon(Icons.lock),
+                    labelText: 'Password',
+                  ),
+                  onChanged: (value) {
+                    setState(() => password = value);
+                  },
+                  validator: (value) => value!.length < 6
+                      ? 'Enter a password 6+ chars long'
+                      : null,
+                  obscureText: true,
                 ),
-                onChanged: (value) {
-                  setState(() => password = value);
-                },
-                validator: (value) =>
-                    value!.length < 6 ? 'Enter a password 6+ chars long' : null,
-                obscureText: true,
-              ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue)),
-                child: const Text(
-                  'Sign In',
-                  style: TextStyle(color: Colors.white),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _auth
+                          .signInWithEmailAndPassword(email, password)
+                          .then((value) {
+                        if (value.runtimeType == String) {
+                          setState(() {
+                            error = value;
+                          });
+                        }
+
+                        return;
+                      });
+                    }
+                  },
                 ),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _auth
-                        .signInWithEmailAndPassword(email, password)
-                        .then((value) {
+                const SizedBox(height: 20.0),
+                Text(
+                  error,
+                  style: const TextStyle(color: Colors.red, fontSize: 12.0),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red)),
+                  child: const Text(
+                    'Sign In with Google',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    _auth.signInWithGoogle().then((value) {
                       if (value.runtimeType == String) {
                         setState(() {
                           error = value;
@@ -86,35 +115,10 @@ class _SignInState extends State<SignIn> {
 
                       return;
                     });
-                  }
-                },
-              ),
-              const SizedBox(height: 20.0),
-              Text(
-                error,
-                style: const TextStyle(color: Colors.red, fontSize: 12.0),
-              ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.red)),
-                child: const Text(
-                  'Sign In with Google',
-                  style: TextStyle(color: Colors.white),
+                  },
                 ),
-                onPressed: () {
-                  _auth.signInWithGoogle().then((value) {
-                    if (value.runtimeType == String) {
-                      setState(() {
-                        error = value;
-                      });
-                    }
-
-                    return;
-                  });
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
